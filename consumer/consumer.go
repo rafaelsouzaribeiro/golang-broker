e49155c502ca5b9e3d91735f7a6b2634d9b05379
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-03-15 12:32:48 -0300

    Module apache kafka

---
+++ consumer/consumer.go
@@ -0,0 +1,103 @@
+package consumer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/IBM/sarama"
+)
+
+type consumerGroupHandler struct {
+	Messages []string
+}
+
+type Consumer struct {
+	errors  chan error
+	handler *consumerGroupHandler
+	brokers []string
+	groupId string
+	topics  []string
+	config  *sarama.Config
+}
+
+// Cleanup implements sarama.ConsumerGroupHandler.
+func (c *consumerGroupHandler) Cleanup(s sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// Setup implements sarama.ConsumerGroupHandler.
+func (c *consumerGroupHandler) Setup(s sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (c *consumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+
+	for msg := range claim.Messages() {
+
+		fmt.Printf("Mensagem recebida: %s\n", msg.Value)
+		c.Messages = append(c.Messages, string(msg.Value))
+
+		session.MarkMessage(msg, "") // Marca a mensagem como processada
+
+	}
+
+	return nil
+}
+
+func NewConsumer(brokers []string, groupId string, topics []string, config *sarama.Config) *Consumer {
+	return &Consumer{
+		brokers: brokers,
+		groupId: groupId,
+		topics:  topics,
+		config:  config,
+		errors:  make(chan error),
+	}
+}
+
+func (p *Consumer) GetConsumer() (sarama.ConsumerGroup, error) {
+
+	client, err := sarama.NewConsumerGroup(p.brokers, p.groupId, p.config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (p *Consumer) VerifyConsumer(client sarama.ConsumerGroup) (context.CancelFunc, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	err := client.Consume(ctx, p.topics, p.handler)
+	if err != nil {
+		return cancel, err
+	}
+
+	return cancel, nil
+
+}
+
+func (p *Consumer) VerifyError(client sarama.ConsumerGroup) error {
+	p.errors = make(chan error)
+
+	go func() {
+		for {
+			err := <-client.Errors()
+			if err != nil {
+				// Se houver um erro, feche o canal de erros e encerre a goroutine
+				fmt.Println("Ocorreu algum erro")
+
+				p.errors <- err
+				close(p.errors)
+				return
+			}
+		}
+	}()
+
+	for err := range p.errors {
+		fmt.Println(err.Error())
+		return err // Assuming you want to stop after receiving the first error
+	}
+
+	return nil
+}
---
+++ producer/producer.go
@@ -0,0 +1,56 @@
+package producer
+
+import (
+	"fmt"
+
+	"github.com/IBM/sarama"
+)
+
+type Producer struct {
+	addrs   []string
+	topic   string
+	message sarama.Encoder
+	config  *sarama.Config
+}
+
+func NewProducer(addrs []string, topic string, message sarama.Encoder, config *sarama.Config) *Producer {
+	return &Producer{
+		addrs:   addrs,
+		topic:   topic,
+		message: message,
+		config:  config,
+	}
+}
+
+func (p *Producer) GetProducer() (*sarama.AsyncProducer, error) {
+
+	producer, err := sarama.NewAsyncProducer(p.addrs, p.config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &producer, err
+}
+
+func (p *Producer) SendMessage(producer *sarama.AsyncProducer) error {
+	message := &sarama.ProducerMessage{Topic: p.topic, Value: p.message}
+
+	(*producer).Input() <- message
+
+	select {
+	case success := <-(*producer).Successes():
+		value, err := success.Value.Encode()
+		if err != nil {
+			fmt.Println("Error encoding message:", err)
+			return err
+		}
+
+		fmt.Println("Mensagem produzida:", string(value))
+	case err := <-(*producer).Errors():
+		fmt.Println("Falho para mensagem produzida:", err)
+		return err
+	}
+
+	return nil
+}
