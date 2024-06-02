3dd5260f73d03a16d9e6fd5e5a446a412366975e
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-06-02 18:57:15 -0300

    adding sns-sqs

--- pkg/sns-sqs/sqs/main.go
+++ pkg/sns-sqs/sqs/main.go
@@ -14,44 +14,43 @@ const (
 )
 
 func main() {
-	// Configurar sessão AWS com o LocalStack
+	go Sqs()
+	select {}
+}
+
+func Sqs() {
 	sess := session.Must(session.NewSession(&aws.Config{
 		Endpoint: aws.String("http://localhost:4566"),
 		Region:   aws.String("us-east-1"),
 	}))
 
-	// Criar um cliente SQS
 	svc := sqs.New(sess)
 
-	// Parâmetros para receber mensagens da fila SQS
-	receiveMessageInput := &sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(queueURL),
-		MaxNumberOfMessages: aws.Int64(10), // Número máximo de mensagens a serem recebidas
-		WaitTimeSeconds:     aws.Int64(20), // Tempo de espera (long polling)
-	}
+	for {
+		receiveMessageInput := &sqs.ReceiveMessageInput{
+			QueueUrl:        aws.String(queueURL),
+			WaitTimeSeconds: aws.Int64(20),
+		}
 
-	// Receber mensagens da fila SQS
-	result, err := svc.ReceiveMessage(receiveMessageInput)
-	if err != nil {
-		log.Fatalf("Erro ao receber mensagens: %v", err)
-	}
+		result, err := svc.ReceiveMessage(receiveMessageInput)
+		if err != nil {
+			log.Fatalf("Erro ao receber mensagens: %v", err)
+		}
 
-	// Exibir as mensagens recebidas
-	if len(result.Messages) > 0 {
-		for _, message := range result.Messages {
-			fmt.Printf("Mensagem recebida: %s\n", *message.Body)
+		if len(result.Messages) > 0 {
+			for _, message := range result.Messages {
+				fmt.Printf("Mensagem recebida: %s\n", *message.Body)
 
-			// Apagar a mensagem após o processamento
-			deleteMessageInput := &sqs.DeleteMessageInput{
-				QueueUrl:      aws.String(queueURL),
-				ReceiptHandle: message.ReceiptHandle,
-			}
-			_, err := svc.DeleteMessage(deleteMessageInput)
-			if err != nil {
-				log.Fatalf("Erro ao apagar a mensagem: %v", err)
+				deleteMessageInput := &sqs.DeleteMessageInput{
+					QueueUrl:      aws.String(queueURL),
+					ReceiptHandle: message.ReceiptHandle,
+				}
+				_, err := svc.DeleteMessage(deleteMessageInput)
+				if err != nil {
+					log.Fatalf("Erro ao apagar a mensagem: %v", err)
+				}
 			}
 		}
-	} else {
-		fmt.Println("Nenhuma mensagem recebida.")
 	}
+
 }
